c05d9ec7d245bbb5fb9e8a60ec9b9a7bc7b2936d
MortenJansfort <mortenjansfort>
2015-09-23 12:22:06 +0200

    gogog

---
+++ POS/src/pos/Basket.java
@@ -0,0 +1,27 @@
+package pos;
+
+
+import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Morten
+ */
+public class Basket {
+    
+    private ArrayList<Product> items = new ArrayList<>();
+    
+    public int calcPrice(){
+        return 0;
+    }    
+    
+    public void addProduct(int id){
+        
+    }
+}
---
+++ POS/src/pos/Register.java
@@ -0,0 +1,27 @@
+package pos;
+
+
+import pos.Basket;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Morten
+ */
+public class Register {
+    
+    private Basket basket = new Basket();
+    
+    
+    public int calcReturn(int input){
+        return 0;
+        
+    } 
+    
+    
+}
